f41a8de7a3968b0d29ee36cec0fb957d7305e734
Florian Neukirchen <mail@riannek.de>
2022-12-18 15:31:29 +0100

    Write minerals to hidden input field

--- static/add.js
+++ static/add.js
@@ -1,5 +1,7 @@
 // Magic search
-let input = document.getElementById('mininput');
+const input = document.getElementById('mininput');
+const hidden = document.getElementById('hiddenminerals');
+
 input.addEventListener('input', async function() {
     let response = await fetch('/mineralsearch?q=' + input.value);
     let searchresult = await response.json();
@@ -15,18 +17,25 @@ input.addEventListener('input', async function() {
 
 // Add to list
 
+var mineralsarray = [];
 var addBtns = document.querySelectorAll('.addBtn');
 
 // function triggered by click event
 
 function addtolist(value) {
+  //update invisable input field
+  mineralsarray.push(value)
+  hidden.value = mineralsarray.join()
+
+  //add mineral to visable list
   const ul = document.getElementById('ulminerals');
   const li = document.createElement('li');
   li.innerHTML = value;
+  li.className = "list-group-item";
   ul.appendChild(li);
   // Create delete button
   createBtn(li);
-  // Clear input
+  // Clear input (TODO does not work, why?)
   input.value = '';
   
 }
